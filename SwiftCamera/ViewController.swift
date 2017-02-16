f45948412a8ad4d42336b6bdc55b5bede2c61c59
USER <user@user-no-macbook-pro.local>
2017-02-16 14:49:28 +0900

    swift camera first commit

--- SwiftCamera/ViewController.swift
+++ SwiftCamera/ViewController.swift
@@ -8,7 +8,8 @@
 
 import UIKit
 
-class ViewController: UIViewController {
+class ViewController: UIViewController , UIImagePickerControllerDelegate,UINavigationControllerDelegate{
+    @IBOutlet weak var backImageView: UIImageView!
 
     override func viewDidLoad() {
         super.viewDidLoad()
@@ -20,6 +21,50 @@ class ViewController: UIViewController {
         // Dispose of any resources that can be recreated.
     }
 
+    @IBAction func camera(_ sender: Any) {
+        
+        let souceType:UIImagePickerControllerSourceType = UIImagePickerControllerSourceType.camera
+        
+        if UIImagePickerController.isSourceTypeAvailable(UIImagePickerControllerSourceType.camera){
+            let cameraPicker = UIImagePickerController()
+            
+            cameraPicker.sourceType = souceType
+            cameraPicker.delegate = self
+            
+            self.present(cameraPicker, animated: true, completion: nil)
+            
+        }
+        
+    }
 
+    @IBAction func album(_ sender: Any) {
+        
+        let souceType:UIImagePickerControllerSourceType = UIImagePickerControllerSourceType.photoLibrary
+        
+        if UIImagePickerController.isSourceTypeAvailable(UIImagePickerControllerSourceType.photoLibrary){
+            let cameraPicker = UIImagePickerController()
+            
+            cameraPicker.sourceType = souceType
+            cameraPicker.delegate = self
+            
+            self.present(cameraPicker, animated: true, completion: nil)
+            
+        }
+    }
+    
+    
+    func imagePickerController(_ picker: UIImagePickerController, didFinishPickingMediaWithInfo info: [String : Any]) {
+        
+        if let pickedImage = info[UIImagePickerControllerOriginalImage] as? UIImage{
+            backImageView.image = pickedImage
+            
+            UIImageWriteToSavedPhotosAlbum(pickedImage, self, nil, nil)
+        }
+        
+        picker.dismiss(animated: true, completion: nil)
+    }
+    
+    
+    
 }
 
